3b9d5a2954d8f374a677ac64a476274b1f6e4491
Felice Forby <feliceforby@gmail.com>
2018-04-12 23:30:39 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -5,7 +5,7 @@ def badge_maker(name)
 end
 
 def batch_badge_creator(names)
-  names.map { |name| batch << badge_maker(name) }
+  names.map { |name| badge_maker(name) }
 end
 
 def assign_rooms(speakers)
